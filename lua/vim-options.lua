e88f8302975daa51528bf4eaf95b754efd88612a
Reza Fahlevi <rzfhlv.me@gmail.com>
2024-01-07 22:13:51 +0700

    add alpha splash screen

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,5 +2,12 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.cmd("set number")
 vim.g.mapleader = " "
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+vim.wo.number = true
